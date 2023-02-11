977f5dec4e772cef6bb5b303509e4fd04eb5bb8e
aina4 <95197155+aina4@users.noreply.github.com>
2023-02-11 23:08:23 +0800

    Update editstore.java

--- main/java/com/example/fypg/editstore.java
+++ main/java/com/example/fypg/editstore.java
@@ -1,29 +1,103 @@
 package com.example.fypg;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class editstore extends AppCompatActivity {
 
-    TextView tv, tv2;
-    Button btn;
+    TextView back, logout;
+    ImageView updateImage;
+    Button updateButton;
+    EditText updateName, updateAddress;
+    String name, address;
+    String imageUrl;
+    String key, oldImageURL;
+    Uri uri;
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editstore);
 
-        tv = findViewById(R.id.textView37);
-        tv2 = findViewById(R.id.textView38);
-        btn = findViewById(R.id.button5);
+        updateButton = findViewById(R.id.updateButton);
+        updateName = findViewById(R.id.updateName);
+        updateImage = findViewById(R.id.updateImage);
+        updateAddress = findViewById(R.id.updateAddress);
 
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK){
+                            Intent data = result.getData();
+                            uri = data.getData();
+                            updateImage.setImageURI(uri);
+                        } else {
+                            Toast.makeText(editstore.this, "Gambar tidak dipilih", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            Glide.with(editstore.this).load(bundle.getString("Image")).into(updateImage);
+            updateName.setText(bundle.getString("Name"));
+            updateAddress.setText(bundle.getString("Address"));
+            key = bundle.getString("Key");
+            oldImageURL = bundle.getString("Image");
+        }
+        databaseReference = FirebaseDatabase.getInstance().getReference("Kedai").child(key);
+
+        updateImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                photoPicker.setAction(Intent.ACTION_GET_CONTENT);
+                activityResultLauncher.launch(photoPicker);
+            }
+        });
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                Intent intent = new Intent(editstore.this, crud.class);
+                startActivity(intent);
+            }
+        });
         //crud page
-        tv.setOnClickListener(new View.OnClickListener() {
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(editstore.this, crud.class);
@@ -31,19 +105,61 @@ public class editstore extends AppCompatActivity {
             }
         });
         //homepage (logout)
-        tv2.setOnClickListener(new View.OnClickListener() {
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(editstore.this, homepage.class);
                 startActivity(intent);
             }
         });
-        //homepage after updating store
-        btn.setOnClickListener(new View.OnClickListener() {
+    }
+    public void saveData(){
+        storageReference = FirebaseStorage.getInstance().getReference().child("Store Images").child(uri.getLastPathSegment());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(editstore.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(editstore.this, crud.class);
-                startActivity(intent);
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isComplete());
+                Uri urlImage = uriTask.getResult();
+                imageUrl = urlImage.toString();
+                updateData();
+                dialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                dialog.dismiss();
+            }
+        });
+    }
+    public void updateData(){
+        name = updateName.getText().toString().trim();
+        address = updateAddress.getText().toString().trim();
+
+        Store store = new Store(name, address, imageUrl);
+
+        databaseReference.setValue(store).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    StorageReference reference = FirebaseStorage.getInstance().getReferenceFromUrl(oldImageURL);
+                    reference.delete();
+                    Toast.makeText(editstore.this, "Updated", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(editstore.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
